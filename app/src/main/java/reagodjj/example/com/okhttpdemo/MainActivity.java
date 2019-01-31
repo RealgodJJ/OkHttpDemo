0ba03d75cb4bd33b9f47b19236fe365e11c74e99
maaonan <xiaozhiqiu13@sina.com>
2019-01-31 16:43:38 +0800

    OkHttp Get Request.

--- app/src/main/java/reagodjj/example/com/okhttpdemo/MainActivity.java
+++ app/src/main/java/reagodjj/example/com/okhttpdemo/MainActivity.java
@@ -2,17 +2,77 @@ package reagodjj.example.com.okhttpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private OkHttpClient okHttpClient;
+    public static final String TAG = "RealgodJJ";
+    private TextView tvGetInformation;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        okHttpClient = new OkHttpClient();
+        tvGetInformation = findViewById(R.id.tv_get_information);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.m_get:
+                get();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void get() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                Request.Builder builder = new Request.Builder();
+                builder.url("https://raw.githubusercontent.com/square/okhttp/master/README.md");
+                Request request = builder.build();
+                Log.d(TAG, "run: " + request);
+                Call call = okHttpClient.newCall(request);
+                try {
+                    Response response = call.execute();
+                    if (response.isSuccessful()) {
+                        final String bodyString = response.body() != null ? response.body().string() : null;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvGetInformation.setText(bodyString);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executor.shutdown();
     }
 }
